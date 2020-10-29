65c5c383ee6709dc5fe4d4d6ff17e1cb0b3d5514
Huslen Caulfield <hoodiepielp@gmail.com>
2020-10-30 01:35:44 +0800

    home design

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -12,31 +12,34 @@
     @auth
     <a href="http://localhost:8000/shop" class="menuOptions">Shop</a>
     <a href="http://localhost:8000/sell" class="menuOptions">Sell</a>
+    <form>
+        <input type="text" id="search-bar" name="search" placeholder="Search..">
+    </form>
     <a href="http://localhost:8000/profile" class="userReg">Profile</a>
      <a class="userReg" href="#" onclick="event.preventDefault();document.getElementById('logout-form').submit();">Logout</a>
         <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
             @csrf
         </form>
     @else
+    <form>
+        <input type="text" id="search-bar" name="search" placeholder="Search..">
+    </form>
     <a href="http://localhost:8000/login" class="userReg">Sign in</a>
     <a href="http://localhost:8000/register" class="userReg">Register</a>
     @endauth
 </div><br>
-	<h1 id="headText">WELCOME! to Thrift Shop Mongolia</h1>
-	<div>
+<div class="container">
 	<img src="images/pic1.jpg" class="homeImg" id="rotate" style="margin-left: 5%; width: 400px; height: 300px;">
 	<img src="images/pic2.jpg" class="homeImg" style="margin-left: 15%; width: 400px; height: 300px; ">
-	</div><br><br><br><br><br>
+    <img src="images/pic1.jpg" class="homeImg" id="rotate" style="margin-left: 5%; width: 400px; height: 300px;">
+	<img src="images/pic2.jpg" class="homeImg" style="margin-left: 15%; width: 400px; height: 300px; ">
+    <img src="images/pic1.jpg" class="homeImg" id="rotate" style="margin-left: 5%; width: 400px; height: 300px;">
+	<img src="images/pic2.jpg" class="homeImg" style="margin-left: 15%; width: 400px; height: 300px; ">
 
-	<div>
-		<h2 style="color: #FFFFFF; text-align: center; font-size: 60px">Thrift shop гэж юу вэ?</h2>
-		<blockquote style="color: #FFFFFF; text-align: center; font-size: 40px;">
-			Энэ бол та өөрт хэрэгцээгүй бараагаа зарах эсвэл өөрийн<br> хэрэгцээтэй зүйлсийг хямд үнээр олж авах орон зай юм.
-		</blockquote>
-	</div>
-<!--<footer id="footer" align="center" style="bottom: 0;">
+</div>
+<footer id="footer">
   <h3>Thrift shop</h3>
-  <p>© 2019 Sleepless Zombies Co.ltd. All Right Reserved. </p>  -->
+  <p>© 2019 Sleepless Zombies Co.ltd. All Right Reserved. </p>
 </footer>
 </body>
 </html>
