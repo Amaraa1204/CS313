988a72c08b0858491613c9098187b03031dee411
Huslen Caulfield <hoodiepielp@gmail.com>
2020-12-04 22:14:40 +0800

    sell design

--- resources/views/sell.blade.php
+++ resources/views/sell.blade.php
@@ -40,66 +40,71 @@
     @endauth
 </div><br>
 
-<div class="div" >
-         <div class="head">
-            <div class="head1"><b>Зарах</b></div>
-              	<form action="/publish" method="post" enctype="multipart/form-data">
-                    {{ csrf_field() }}
-                  	Хувцасны нэр:<br>
-                  	<input type="text" name="name"><br>
-                      @error('name')
-                                <span class="invalid-feedback" role="alert">
-                                    <strong>{{ $message }}</strong>
-                                </span>
-                            @enderror
-                  	Хувцасны төрөл: <br>
-                  	<input id="type" type="radio" name="type" value="shirt">Shirt<br>
-                    <input id="type" type="radio" name="type" value="hat">Hat<br>
-                    <input id="type" type="radio" name="type" value="trouser">Trousers<br>
-                    <input id="type" type="radio" name="type" value="jacket">Jackets<br>
-                    <input id="type" type="radio" name="type" value="shoe">Shoes<br>
-                    @error('type')
-                                <span class="invalid-feedback" role="alert">
-                                    <strong>{{ $message }}</strong>
-                                </span>
-                            @enderror
-                    Ашигласан хугацаа(сар):<br>
-                  	<input type="number" name="time"><br>
-                      @error('time')
-                                <span class="invalid-feedback" role="alert">
-                                    <strong>{{ $message }}</strong>
-                                </span>
-                            @enderror
-                  	Үнэ(төгрөг):<br>
-                  	<input type="number" name="price"><br>
-                      @error('price')
-                                <span class="invalid-feedback" role="alert">
-                                    <strong>{{ $message }}</strong>
-                                </span>
-                            @enderror
-                  	Тайлбар:<br>
-                  	<input type="text" name="def" style="height: 100px"><br>
-                      @error('def')
-                                <span class="invalid-feedback" role="alert">
-                                    <strong>{{ $message }}</strong>
-                                </span>
-                            @enderror
-                  	Зураг:<br><br>
-                        <label class="button" for="photo">Choose file</label>
-                        <div class="col-md-6">
-                            <input id="photo" type="file" class="form-control @error('photo') is-invalid @enderror" name="photo" required autocomplete="photo" id="upload-photo" style="opacity: 0;" multiple>
+<div class="head1"><b>Зарах</b></div>
+<div class="sell-form">  
+  <form action="/publish" method="post" enctype="multipart/form-data">
+  {{ csrf_field() }}
+  <div class="row">
+    <div class="column">
+      <label for="name">Хувцасны нэр:</label>
+       <input type="text" name="name" id="name">
+        @error('name')
+          <span class="invalid-feedback" role="alert">
+            <strong>{{ $message }}</strong>
+          </span>
+          @enderror
+          <label for="type">Хувцасны төрөл:</label>
+          <select name="type" id="type">
+            <option value="shirt" selected>Shirt</option>          
+            <option value="hat">Hat</option>          
+            <option value="trouser">Trousers</option>          
+            <option value="jacket">Jacket</option>          
+            <option value="shoes">Shoes</option>           
+          </select>
+             @error('type')
+               <span class="invalid-feedback" role="alert">
+                   <strong>{{ $message }}</strong>
+               </span>
+               @enderror<br>
+              <label for="time">Ашигласан хугацаа(сар):</label>
+               <input id="time" type="number" name="time">
+               @error('time')
+               <span class="invalid-feedback" role="alert">
+                <strong>{{ $message }}</strong>
+              </span>
+              @enderror
+               <label for="price">Үнэ(төгрөг):</label>
+              <input type="number" name="price" id="price">
+              @error('price')
+              <span class="invalid-feedback" role="alert">
+                <strong>{{ $message }}</strong>
+              </span>
+              @enderror
+            </div>
 
-                            @error('photo')
-                                <span class="invalid-feedback" role="alert">
-                                    <strong>{{ $message }}</strong>
-                                </span>
-                            @enderror
-                        </div>
-                    
-                  	<input type="submit" value="Байршуулах" name="submit" class="button"></div>
-              	</form>
-         </div>
-      </div>
+          <div class="column">     
+               <label for="def">Тайлбар:</label>
+              <input id="def" type="text" name="def" style="height: 100px"><br>
+              @error('def')
+              <span class="invalid-feedback" role="alert">
+                <strong>{{ $message }}</strong>
+              </span>
+              @enderror
+              Зураг:<br><br>
+              <label class="button" for="photo">Choose file</label>
+              <div class="col-md-6">
+                <input id="photo" type="file" class="form-control @error('photo') is-invalid @enderror" name="photo" required autocomplete="photo" id="upload-photo" style="opacity: 0;" multiple>
+
+                @error('photo')
+                <span class="invalid-feedback" role="alert">
+                  <strong>{{ $message }}</strong>
+                </span>
+                @enderror
+                <input type="submit" value="Байршуулах" name="submit" class="button"></div>
+              </div>
+            </div>
+        </form>
+</div>
 <!--<footer id="footer" align="center">
   <h3>Thrift shop</h3>
   <p>© 2019 Sleepless Zombies Co.ltd. All Right Reserved. </p>  -->
